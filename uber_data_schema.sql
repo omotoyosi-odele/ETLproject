f7f713aabe2c3e4694686db861ce2077ae21efe2
Omotoyosi Odele <59804852+omotoyosi-odele@users.noreply.github.com>
2020-04-29 21:48:43 -0400

    Updating documents

---
+++ uber_data_schema.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE "uber_rides" (
+    "id" varchar   NOT NULL,
+    "distance" float   NOT NULL,
+    "source" varchar   NOT NULL,
+    "destination" varchar   NOT NULL,
+    "price" float   NOT NULL,
+    "time_stamp" int(11)   NOT NULL,
+    "name" varchar   NOT NULL,
+    CONSTRAINT "pk_uber_rides" PRIMARY KEY (
+        "id"
+     )
+);
+
